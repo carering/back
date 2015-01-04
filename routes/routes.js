a0511093bf7b1167cbcb35cc43e6f8ebc324c92f
James Stiehl <jstiehl@gs-mbp15-james.local>
2015-01-04 15:46:21 -0800

    initial route setup

---
+++ routes/routes.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+var mysql = require('mysql');
+
+var router = express.Router();
+
+module.exports = function(config){
+	var pool = mysql.createPool(config.mysql);
+
+	router.route("/")
+		.get(function(req,res){
+			res.send("Hello James!");
+		})
+}
