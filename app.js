089925df6fd668a11f0482536c923d2665d25fd3
James Stiehl <jstiehl@gs-mbp15-james.local>
2015-01-04 15:50:12 -0800

    added gitignore

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var app = express();
+var routes = require("./routes/routes");
+var config = require("./config.json")
+
+
+app.use(bodyparser.json());
+
+app.use(routes);
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
-})
+});
 
 var server = app.listen(process.env.PORT || 3000, function () {
 
