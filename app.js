20fd76c1736d004071152d4c6fef1f80810eb787
James Stiehl <jstiehl@gs-mbp15-james.local>
2015-01-04 17:13:08 -0800

    added some additional routing for test

--- app.js
+++ app.js
@@ -2,12 +2,9 @@ var express = require('express');
 var app = express();
 var config = require("./config.json");
 var routes = require("./routes/routes")(config);
-
+console.log(config);
 app.use(routes);
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!')
-// });
 
 var server = app.listen(process.env.PORT || 3000, function () {
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,42 @@
 var express = require('express');
 var bodyparser = require('body-parser');
 var mysql = require('mysql');
+var async = require('async');
 
 var router = express.Router();
 
 module.exports = function(config) {
 	var pool = mysql.createPool(config.mysql);
+	console.log(config.mysql);
+	router.use(bodyparser.json());
 
 	router.route("/")
 		.get(function(req,res){
 			res.send("Hello James!");
+		});
+
+	router.route("/register")
+		.post(function(req, res){
+			var data = req.body;
+
+			pool.query('SELECT email from t_users where id=?', [[1]], function(err, results){
+				if(err) console.log(err);
+				console.log(results);
+				res.send("Registration information is: " + JSON.stringify(data)+"\n");
+			});	
+		});
+
+	router.route("/profile/:email")
+		.get(function(req, res){
+			var email = req.params.email;
+			console.log(email);
+
+			pool.query('SELECT first_name, last_name, email, phone, address, rings, brownies from t_users where email=?', [[email]], function(err, results){
+				if(err) console.log(err);
+				console.log(results);
+				res.send("Registration information is: " + JSON.stringify(results[0])+"\n");
+			});	
+
 		});
 
 	return router;
